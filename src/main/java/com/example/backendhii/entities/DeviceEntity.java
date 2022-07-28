61ec4f679dfc9b032eb745d234b720b7d296012d
nguyenlannnn <lannhatthuy@gmail.com>
2022-07-28 23:22:14 +0700

    entity

---
+++ src/main/java/com/example/backendhii/entities/DeviceEntity.java
@@ -0,0 +1,34 @@
+package com.example.backendhii.entities;
+
+import com.example.backendhii.basess.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity(name = "device")
+public class DeviceEntity extends BaseEntity {
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String userAgent;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String accessToken;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String refreshToken;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+}
