ef1059d9a32d51bb5f912fcd20c97297d3e34921
nguyenlannnn <lannhatthuy@gmail.com>
2022-07-30 15:15:21 +0700

    update

--- src/main/java/com/example/backendhii/entities/RoleEntity.java
+++ src/main/java/com/example/backendhii/entities/RoleEntity.java
@@ -1,15 +1,14 @@
 package com.example.backendhii.entities;
 
 import com.example.backendhii.basess.BaseEntity;
+import com.example.backendhii.enums.RoleEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.Collection;
 
 @Getter
@@ -20,8 +19,9 @@ import java.util.Collection;
 @Entity(name = "role")
 public class RoleEntity extends BaseEntity {
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, unique = true)
-    private String name;
+    private RoleEnum name;
 
     @ManyToMany(mappedBy = "roles")
     private Collection<UserEntity> users;
