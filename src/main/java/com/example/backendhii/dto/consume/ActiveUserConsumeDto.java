c06f6d4f83f5b0c19488d342ea4b5e802f209796
cuongnh2k <cuongnh2k@gmail.com>
2022-08-09 23:13:54 +0700

    update

---
+++ src/main/java/com/example/backendhii/dto/consume/ActiveUserConsumeDto.java
@@ -0,0 +1,15 @@
+package com.example.backendhii.dto.consume;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ActiveUserConsumeDto implements Serializable {
+
+    private Integer code;
+}
